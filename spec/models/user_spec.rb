02a43af9a2dc4d6df2f8c91d74bf735cc8eda1b8
digitalcake <joshc@digitalcak.es>
2017-05-26 21:36:45 -0700

    add user unit test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "#admin?" do
+    it "check is user is an admin" do
+      expect(User.new(admin: true).admin?).to be true
+    end
+
+    it "defaults to false" do
+      expect(User.new.admin?).to be false
+    end
+  end
 end
